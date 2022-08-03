2b9a1e09f9a0468fa3be9886a8720057d361cb4a
Elliot Harris <elliot@Elliots-MacBook-Air.local>
2022-08-03 22:27:44 +0100

    First commit, base without functionality

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,13 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// namespace Chess {
+//     public class BoardController : MonoBehaviour {
+//         public BoardController(PosInfo posInfo) {
+//             for (int i = 0; i < 64; i++) {}
+//             
+//         }
+//     }
+// }
+
