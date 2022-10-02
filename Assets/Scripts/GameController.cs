a2a6008221e62d92b118b7e4da56d871709dc14d
Elliot Harris <elliot.harris03@gmail.com>
2022-10-02 15:56:58 +0100

    Storing tile properties in tile (TileProps)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -23,52 +23,58 @@ public class GameController : MonoBehaviour {
 
     private GameObject _parentBoard;
     private GameObject _parentPieces;
-
-    // private enum ObjectLookup {
-    //     BKing = 1,
-    //     BQueen = 2,
-    //     BRook = 3,
-    //     BBishop = 4,
-    //     BKnight = 5,
-    //     BPawn = 6,
-    //     WKing = 11,
-    //     WQueen = 12,
-    //     WRook = 13,
-    //     WBishop = 14,
-    //     WKnight = 15,
-    //     WPawn = 16,
-    //         
-    //     LightSquare = 100,
-    //     DarkSquare = 101
-    // }
     
-    // private Dictionary<int, GameObject> mapSprite = new Dictionary<int, GameObject>() {
-    //     {(int)ObjectLookup.BKing, bKing}, {Piece.Queen, queen}, {Piece.Rook, rook}, {Piece.Bishop, bishop}, 
-    //     {Piece.Knight, knight}, {Piece.Pawn, pawn}
-    // };
-        
+    private TileProps[] _tilesProps;
+
+    private static readonly int[,] RotationMatrix = {
+        { 0, 8, 16, 24, 32, 40, 48, 56 },
+        { 1, 9, 17, 25, 33, 41, 49, 57 },
+        { 2, 10, 18, 26, 34, 42, 50, 58 },
+        { 3, 11, 19, 27, 35, 43, 51, 59 },
+        { 4, 12, 20, 28, 36, 44, 52, 60 },
+        { 5, 13, 21, 29, 37, 45, 53, 61 },
+        { 6, 14, 22, 30, 38, 46, 54, 62 },
+        { 7, 15, 23, 31, 39, 47, 55, 63 }
+    };
+    
     private void Start() {
         _parentBoard = GameObject.Find("Board");
         _parentPieces = GameObject.Find("Pieces");
-        DrawBoard();
+        _tilesProps = DrawBoard();
         FenUtility fen = new FenUtility();
         fen.FenToBoard("default");
-        // fen.OutputInfo();
+        //fen.OutputInfo();
         DrawPieces(fen);
-            
+        for (int i = 0; i < 64; i++) {
+            Debug.Log(i + " : " + _tilesProps[i].id);
+        }
+        
     }
     
     // column = file (1, 2...), row = rank (a, b...)
-    private void DrawBoard() {
+    private TileProps[] DrawBoard() {
+        TileProps[] tilesProps = new TileProps[64];
         for (int file = 0; file < 8; file++) {
             for (int rank = 0; rank < 8; rank++) {
+                
+                int rPos = RotationMatrix[file, rank];
                 bool isLight = (file + rank) % 2 != 0;
                 
-                Vector2 position = new Vector2(file, rank);
                 GameObject obj = isLight ? lightSquare : darkSquare;
+                
+                TileProps tileProps = (TileProps)obj.GetComponent("TileProps");
+                tileProps.id = (rPos); // assign id
+                tileProps.pos = intToPos(tileProps.id);
+                tileProps.vec = posToVec(tileProps.pos);
+                Vector2 position = tileProps.vec;
+                
+                tilesProps[rPos] = tileProps;
+                
                 Create(obj, position);
             }
         }
+
+        return tilesProps;
     }
 
     private void DrawPieces(FenUtility fen) {
@@ -117,11 +123,14 @@ public class GameController : MonoBehaviour {
             newObj.transform.parent = _parentPieces.transform; 
         }
     }
-        
-    // TODO: convert int straight to Vector2?
+    // ----------------------- //
+    //    Utility functions    //
+    
+    // The two functions below will usually be called together, but are separate so the square number can be identified
     public int[] intToPos(int numPos) {
         int[] fileRank = new int[2];
-        // case 64: 63/8 = 7 floored, 63 % 8 = r7
+        // case 63: 63/8 = 7 floored, 63 % 8 = r7
+        // case 44: 44/8 = 5 floored, 44 % 8 = r4
         // case 1: 1/8 = 0 floored, 1 % 8 = r1
         // case 0: 0, r0
         int file = numPos % 8;
@@ -141,4 +150,9 @@ public class GameController : MonoBehaviour {
 
         return new Vector2(column, row);
     }
+
+    // public int toRawNum(int a, int b) {
+    //     int eval = RotationMatrix[a, b];
+    //     return eval;
+    // }
 }
---
+++ Assets/Scripts/TileProps.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileProps : MonoBehaviour {
+    // use get / set ?
+    public int id;
+    public int[] pos;
+    public Vector2 vec;
+}
