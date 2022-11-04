e140eb1d6108c4394ef87cba9f6e85d2b74f782f
Elliot Harris <elliot.harris03@gmail.com>
2022-11-04 13:41:28 +0000

    comitting current state for transfer to mac for travel

--- Assets/Scripts/FenUtility.cs
+++ Assets/Scripts/FenUtility.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FenUtility {
@@ -12,7 +13,7 @@ public class FenUtility {
     public const string StartFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     public static PosInfo _posInfo;
 
-    public static  Fen fenObj;
+    public static Fen fenObj;
 
     // private static string _lastFen;
         
@@ -102,7 +103,11 @@ public class PosInfo {
 public readonly struct Fen {
     public readonly string positions;
     public readonly ActiveColour activeColour;
-    public readonly string castling;
+    public readonly int[] castling;
+    Dictionary<char, PieceType> mapPiece = new () {
+        {'k', PieceType.King}, {'q', PieceType.Queen}, {'r', PieceType.Rook}, {'b', PieceType.Bishop}, 
+        {'n', PieceType.Knight}, {'p', PieceType.Pawn}
+    };
     public readonly string enPassant; // give default 
     public readonly int halfClock;
     public readonly int fullClock;
@@ -117,7 +122,8 @@ public readonly struct Fen {
 
     public Fen(string[] fenArray) {
         var failCount = 0;
-        
+        char[] castleChars = { 'K', 'Q', 'k', 'q' };
+        castling = new []{ 0, 0, 0, 0 };
         positions = fenArray[0];
         
         if (!char.TryParse(fenArray[1], out var tmpColour)) { failCount++; }
@@ -133,15 +139,34 @@ public readonly struct Fen {
                 failCount++;
                 break;
         }
-        castling = fenArray[2];
-        if (string.IsNullOrWhiteSpace(castling)) {
-            castling = "-"; }
+        
+        int count = 0;
+        // castling = fenArray[2];
+        if (string.IsNullOrWhiteSpace(fenArray[2])) {
+            
+        }
+        else {
+            castling = new[] { 0, 0, 0, 0 };
+            foreach (var c in fenArray[2]) {
+                if (castleChars.Contains(c)) {
+                    castling[count] = 1;
+                    count++;
+                }
+                else if (char.IsDigit(c)) {
+                    int tmp = c - '0';
+                    for (int i = 0; i < tmp; i++) {
+                        castling[count] = 0;
+                        count++;
+                    }
+                }
+            }
+        }
         enPassant = fenArray[3];
         if (string.IsNullOrWhiteSpace(enPassant)) {
             enPassant = "-"; }
         if (!int.TryParse(fenArray[4], out halfClock)) { failCount++; }
         if (!int.TryParse(fenArray[5], out fullClock)) { failCount++; }
-
+        
         success = failCount == 0;
         OutputFen();
     }
